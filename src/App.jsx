07c8445cc2a5931bd65093071564a6dab6688526
ruranjo <rubens.rangel.job@gmail.com>
2023-07-26 08:34:15 -0400

    navegation bar done and finish

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,13 @@
 
 import './App.css'
+import Navbar from './components/Navbar'
 
 function App() {
  
 
   return (
     <>
-      holamundo
+      <Navbar/>
     </>
   )
 }
---
+++ src/components/Navbar.jsx
@@ -0,0 +1,26 @@
+import React from 'react'
+import logo from '../assets/brand_logo.png';
+
+const Navbar = () => {
+  
+  return (
+    <div className='navbar-containe'>
+        <nav className='navbar'>
+            <div className="logo">
+                <img src={logo} alt="logo" />
+            </div>
+        
+        <ul>
+            <li ><a href="#">Menu</a></li>
+            <li ><a href="#">Location</a></li>
+            <li ><a href="#">About</a></li>
+            <li ><a href="#">Contact</a></li>
+        </ul>
+        <button>login</button>
+        </nav>
+    </div>
+    
+  )
+}
+
+export default Navbar;
